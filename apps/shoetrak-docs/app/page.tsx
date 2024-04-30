c0cff8f0ed3b1968695821c6d7793c4a3203dd7d
sistemas-shoetrak <168301560+sistemas-shoetrak@users.noreply.github.com>
2024-04-30 19:25:50 -0300

    chore: changing folder names in apps (#3)
    Co-authored-by: Victor Varela <victorvarela.cell@gmail.com>

--- apps/shoetrak-docs/app/page.tsx
+++ apps/shoetrak-docs/app/page.tsx
@@ -57,7 +57,7 @@ export default function Page(): JSX.Element {
       <div className={styles.description}>
         <p>
           examples/basic&nbsp;
-          <Code className={styles.code}>docs</Code>
+          <Code className={styles.code}>shoetrak-docs</Code>
         </p>
         <div>
           <a
