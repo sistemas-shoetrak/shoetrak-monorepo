4adfb86452c1d477c7ae9c213e11772c02e281a0
agodiniz <tmmidiatiago@gmail.com>
2024-05-04 17:22:19 -0300

    feat: add authentication provider component

--- apps/shoetrak-app/app/layout.tsx
+++ apps/shoetrak-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@repo/ui/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import AuthProvider from "./_providers/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,11 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
     </html>
   );
 }
